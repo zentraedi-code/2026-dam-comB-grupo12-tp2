28c911f6cd7bf58b11354ae3047b793e3bd554fc
emiliasosa <emilia.sosatomada@gmail.com>
2026-06-13 14:49:32 -0300

    Unifico diseño en registro de usuario

--- app/src/main/java/com/dam/tp2dam/app/ui/clientes/RegistroClienteActivity.kt
+++ app/src/main/java/com/dam/tp2dam/app/ui/clientes/RegistroClienteActivity.kt
@@ -47,7 +47,7 @@ class RegistroClienteActivity : AppCompatActivity() {
         val cliente = helper.getClienteDao().buscarPorDni(dni) ?: return
         clienteEdicion = cliente
 
-        findViewById<TextView>(R.id.txtTitulo)?.text = "Editar Cliente"
+        findViewById<TextView>(R.id.txtTitulo)?.text = "EDITAR CLIENTE"
         btnGuardar.text = "Actualizar"
 
         etNombre.setText(cliente.nombre)
